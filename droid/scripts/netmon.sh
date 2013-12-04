d3e9da6dbc30b99899313e541258332e48c197ae
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2013-12-04 03:55:07 +0000

    - Added usage messages and 'add_help' calls.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@2856 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

--- droid/scripts/netmon.sh
+++ droid/scripts/netmon.sh
@@ -1,11 +1,37 @@
 #!/bin/ash
 
+
 polling_netstat() {
   local pollTime="$1"
   shift
   local nsArgs="$1"
   shift
-  
+
+  # Check args; null-out the options if there's an error.
+  case "$pollTime" in
+      -h|--help)
+          pollTime=''
+          nsArgs=''
+          ;;
+      [0-9]*)
+          :
+          ;;
+      *)
+          pollTime=''
+          nsArgs=''
+          ;;
+  esac
+
+  if [ -z "$pollTime$nsArgs" ]; then
+      echo "usage:  polling_netstat <pauseTime> [<netstatArgs⃨>]"
+      return 1
+  fi
+
+  # Default vals:
+  if [ -z "$nsArgs" ]; then
+      nsArgs="-naut"
+  fi
+
   local now
   while [ -z "" ]; do
     now=`date '+%Y-%m-%d %H:%M:%S'`
@@ -16,6 +42,8 @@ polling_netstat() {
     sleep $pollTime
   done
 }
+add_help polling_netstat \
+  "<pauseTime> [<netstatArgs⃨>]"
 
 
 netstatmon() {
@@ -23,6 +51,34 @@ netstatmon() {
   shift
   local logFile="$1"
   shift
-  
-  polling_netstat "$pollTime" -npaut | tee -a "$logFile"
+
+  # Check args; null-out the options if there's an error.
+  case "$pollTime" in
+      -h|--help)
+          pollTime=''
+          ;;
+      [0-9]*)
+          :
+          ;;
+      *)
+          pollTime=''
+          ;;
+  esac
+
+  if [ -z "$pollTime" -o -z "$logFile" ]; then
+      echo "usage:  netstatmon <pauseTime> <logFile>"
+      echo ""
+      echo "If run as root, adds the '-p' option "
+      echo "to the 'netstat' call."
+      return 1
+  fi
+
+  local netstatOpts="-naut"
+  if [ $UID -eq 0 ]; then
+      netstatOpts="${netstatOpts}p"
+  fi
+
+  polling_netstat "$pollTime" $netstatOpts | tee -a "$logFile"
 }
+add_help netstatmon \
+  "<pauseTime> <logFile>"
