f45aa6ff93a8947548e73321ab9736dac96bbd92
John Weiss <jpweiss@github.com.nospam>
2014-02-05 18:48:00 -0500

    Adding the 'mk-repo' scripts.
    The "mk-repo._*.sh" scripts are now created anytime my 'svn2git_*' tool
    does an export.  The script contains the explicit 'git svn *' command
    run to generate the new git repo.
    
    Each of these "mk-repo._*.sh" scripts documents how the staging-area for
    each subtree was created, in case that staging area needs to be
    recreated.

---
+++ .subtrees/mk-repo._perl.sh
@@ -0,0 +1,6 @@
+# Created using:
+
+git svn clone --include-paths='^jpwTools.pm$' file:///home/candide/src/0_Local_SVN_Repos/perl/trunk perl
+
+# The old way:
+#	--ignore-path='^(?:[^j]|j[^p]).+(?!\.pm)$'
---
+++ .subtrees/mk-repo._sh.scripts.sh
@@ -0,0 +1,6 @@
+# Created using:
+
+git svn clone --include-paths='^(?:(?:droid|env)/.*|(?:file-purger|git-helpers|mount-fusefs|pathtools)\.sh)$' --ignore-paths='^env/.*(?:crontab|Xresources)$' file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk sh.scripts
+
+# The old way:
+#  --ignore-paths='^(?:(?:[^d-gmp]|d[^r]|e[^n]|f[^i]|fi[^l]|g[^i]|gi[^t]|m[^o]|mount-[^f]|p[^a]|pa[^t]).+|env/.+(?:crontab|Xresources))$'
