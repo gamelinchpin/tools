75c024ebf75e97a617999cdf3c612ba6c0dd79cf
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2014-02-27 04:03:44 +0000

    - New:   + Startup script for "Android Terminal", to be sourced as the     'Initial Command'.   + 'bashrc' for bash-specific setup. - Completed the central 'profile'. - Rearranged & cleaned up 'profile.interactive'.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@2907 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

---
+++ droid/scripts/andterm-start.sh
@@ -0,0 +1,85 @@
+# -*- mode: sh; fill-column: 56 -*-
+#
+# This file should be sourced.
+
+
+### Config:  Uncomment the type of shell you want to
+###          use.  Note:  'bash' falls back to
+###          'kbox_shell' when the full, standalone
+###          version of bash isn't installed.
+
+SHELL0=bash
+#SHELL0=kbox_shell
+#SHELL0=ash
+
+
+############
+
+
+PATH=/data/local/bin:$PATH
+export PATH
+
+
+for d in /sdcard/ext /data/local/kbox /sd-ext \
+  /data/sdext2/base.dir \
+  /data/data/jackpal.androidterm/kbox
+do
+ if [ -d $d ]; then
+  dList="$dList $d"
+ fi
+done
+
+for d in $dList; do
+ if [ -e $d/etc/profile ]; then
+  ENV=/data/local/kbox/etc/profile
+  export ENV
+  break
+ fi
+done
+
+
+# Find the directory containing the requested shell
+for d in $dList; do
+ if [ -e $d/bin/$SHELL0 ]; then
+  SHELL_DIR=$d/bin
+  break
+ fi
+done
+
+
+# Want to run bash?
+if [ "$SHELL0" = "bash" ]; then
+ if [ -n "$SHELL_DIR" ]; then
+  if [ -L $SHELL_DIR/bash ]; then
+   bash_is_bb=`ls -l $SHELL_DIR/bin/bash | grep busybox`
+  fi
+
+  if [ -n "$bash_is_bb" ]; then
+   # We found 'bash', but it's a symlink to busybox.
+   # Perform the fallback-scan.
+   SHELL_DIR=''
+ fi
+fi
+
+
+if [ -z "$SHELL_DIR" ]; then
+ # Perform a fallback-scan:
+
+ for f in kbox_shell ash; do
+  for d in $dList; do
+   if [ -e $d/bin/$f ]; then
+    SHELL_DIR=$d/bin
+    SHELL0=$f
+    break
+   fi
+  done
+ done
+fi
+
+if [ -n "$SHELL_DIR" ]; then
+ exec $SHELL_DIR/$SHELL0
+fi
+
+# else:
+# Fall of the end of the file and keep the startup-shell
+# that sourced this file.
