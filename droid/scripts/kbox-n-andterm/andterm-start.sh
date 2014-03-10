560f33dcdc3ac2ea9a9b9d3d3edfa97eec5c9642
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2014-03-10 21:24:27 +0000

    - Moving the scripts specific to KBOX & Android Terminal setup to   their own subdir. - Renaming 'setup-kbox.sh' to 'setup-overlay.sh', since this script   will soon be more general.   + The old implementation of 'setup-kbox.sh' ceases to be after this     revision.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@2916 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

---
+++ droid/scripts/kbox-n-andterm/andterm-start.sh
@@ -0,0 +1,186 @@
+# -*- mode: sh; fill-column: 56; sh-basic-offset: 2 -*-
+#
+# This file should be sourced.
+
+
+###
+### Config
+###
+
+
+###          Uncomment the type of shell you want to
+###          use.  Note:  'bash' falls back to
+###          'kbox_shell' when the full, standalone
+###          version of bash isn't installed.
+###
+###          If you want this script to find your
+###          non-'busybox'-'bash', consider renaming or
+###          symlinking it to the filename
+###          'bash-standalone'.
+
+#SHELL0=bash
+#SHELL0=bash-standalone
+#SHELL0=kbox_shell
+#SHELL0=ash
+#SHELL0=###FOR::MOD::DURING::INSTALL###
+
+
+###          Default 'etc' Dir.
+###          Leave commented out if you want to scan
+###          for it instead.
+
+#ENV_DIR=/data/local/etc
+#ENV_DIR=###FOR::MOD::DURING::INSTALL###
+
+
+###          Where the '$SHELL0' is located.
+###          Uncomment and set if you have the same
+###          shell in several locations, but want to use
+###          a particular one in a specific directory.
+###          Leave commented out if you want to scan for
+###          it instead.
+
+#SHELL_DIR=/data/sdext2/this/is/an/example
+#SHELL_DIR=###FOR::MOD::DURING::INSTALL###
+
+
+###          Some extra directories to include in the
+###          scans for 'etc/profile' and '$SHELL0'.
+###
+###          The scan *always* starts with
+###          '/data/local/kbox' and '/data/local',
+###          followed by the "usual mountpoints" for the
+###          sdcard's second partition.  The paths you
+###          provide here are checked next.  Lastly,
+###          '/data/data/jackpal.androidterm/kbox' and
+###          '/data/data/jackpal.androidterm' are
+###          checked.
+###
+###          Note:  The directories in this envvar don't
+###          need to exist.  That's checked for you, and
+###          nonexistent directory names omitted.
+###
+###          Modify to taste, or comment out.  [However, it's safe
+###          to leave it.]
+
+SCAN_DIRS="/sdcard/ext /data/sdext2/base.dir"
+
+
+############
+
+
+PATH=/data/local/bin:$PATH
+export PATH
+
+
+# Include the user's preferred location.
+for d in /data/local/kbox /data/local /sd-ext \
+  /system/sd /data/sdext2 $SCAN_DIRS \
+  /data/data/jackpal.androidterm/kbox \
+  /data/data/jackpal.androidterm
+do
+  if [ -d $searchDirs ]; then
+    searchDirs="$searchDirs $d"
+  fi
+done
+
+
+# Look in '$ENV_DIR' first, then continue the scan if
+# that fails.
+for d in $ENV_DIR $searchDirs; do
+  # Make sure we don't end up looking in "${foo}/etc/etc":
+  d=${d%/etc}
+
+  if [ -e $d/etc/profile ]; then
+    etc_profile=$d/etc/profile
+    break
+  fi
+done
+
+if [ -n "$etc_profile" ]; then
+  ENV="$etc_profile"
+  export ENV
+fi
+
+
+# Find the directory containing the requested shell
+for d in $searchDirs; do
+  if [ -e $d/bin/$SHELL0 ]; then
+    SHELL_DIR=$d/bin
+    break
+  fi
+done
+# Check for any built-in version of 'bash' & 'ash'.
+if [ -z "$SHELL_DIR" -a "$SHELL0" != "kbox_shell" ]; then
+  for d in /system/xbin /system/bin; do
+    if [ -e $d/$SHELL0 ]; then
+      SHELL_DIR=$d
+      break
+    fi
+  done
+fi
+
+
+# Want to run bash?
+if [ "$SHELL0" = "bash" ]; then
+
+  # Did we *find* a 'bash' executable?
+  if [ -n "$SHELL_DIR" ]; then
+
+    # Well, let's see if it's standalone or busybox-based.
+    if [ -L $SHELL_DIR/bash ]; then
+      bash_is_bb=`ls -l $SHELL_DIR/bash 2>&1 | grep busybox`
+    fi
+
+    if [ -n "$bash_is_bb" ]; then
+      # We found 'bash', but it's a symlink to busybox.
+      # Perform the fallback-scan.
+      SHELL_DIR=''
+    else
+      # Set this envvar, which we'll use when we start the shell
+      BASH_ENV=$ENV
+      export BASH_ENV
+    fi
+  fi
+
+elif [ "$SHELL0" = "bash-standalone" ]; then
+  # Non-busybox-based 'bash'.
+
+  # Set this envvar, which we'll use when we start the shell
+  BASH_ENV=$ENV
+  export BASH_ENV
+fi
+
+
+if [ -z "$SHELL_DIR" ]; then
+  # We didn't find the preferred shell.
+  # Perform a fallback-scan:
+
+  for f in kbox_shell ash; do
+    for d in $searchDirs; do
+      if [ -e $d/bin/$f ]; then
+        SHELL_DIR=$d/bin
+        SHELL0=$f
+        break
+      fi
+    done
+
+    if [ -n "$SHELL_DIR" ]; then
+      break
+    fi
+  done
+fi
+
+
+# Replace the current shell with the one we found [*if*
+# we found one]:
+if [ -n "$SHELL_DIR" ]; then
+  SHELL=$SHELL_DIR/$SHELL0
+  export SHELL
+
+  exec $SHELL ${BASH_ENV:+--rcfile} ${BASH_ENV}
+fi
+
+# else:
+# Fall of the end of the file and keep the startup-shell
+# that sourced this file.
